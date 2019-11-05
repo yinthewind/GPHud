251b9fa7d7ca2eb64c27fe93ced88836696f12c0
Han Yin <yinthewater@gmail.com>
2019-11-04 16:57:39 -0800

    listen to all events under viewPort

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,1 +1,15 @@
-window.alert("hello, GP Hud")
+const viewPort = document.getElementById('viewPort');
+const config = { attributes: true, childList: true, subtree: true };
+
+const callback = function(mutationList, observer) {
+	for(let mutation of mutationList) {
+		if (mutation.type == 'childList') {
+			console.log(mutation);
+		} else if (mutation.type == 'attributes') {
+			console.log(mutation);
+		}
+	}
+}
+
+const observer = new MutationObserver(callback);
+observer.observe(viewPort, config);
